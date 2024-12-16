caf37956418683d6ac891daaf1af18ba38d42505
Pine1611 <iampine1611@gmail.com>
2024-12-16 22:17:52 +0700

    FE20 - create conversion formula / fix some css

--- 20-temperature-converter/README.md
+++ 20-temperature-converter/README.md
@@ -9,6 +9,17 @@ Project based on [Temperature Converter][temperature-converter] from [Roadmap Id
 -   **Status**: DONE 🎉
 -   **Demo**: [here][demo-link] 🚀
 
+## Notes
+
+-   In this project, its just have a converter for only temperature and only use client js, if you want to see the converter between units of other measurement systems please check my backend projects. I have built an API for converter of measurement systems by ExpressJS.
+-   I have done with more converter like: Length Conversion, Weight Conversion, Temperature Conversion and I still working more on that project, please check at [here][be03].
+
+> All calculation formulas based on my research and created through Wikipedia and Cuemath site
+> Please read these links for more detail:
+> Wikipedia: https://en.wikipedia.org/wiki/Metric_system
+> CueMath: https://www.cuemath.com/measurement/
+> If you copy and use my calculation formulas, please cite the source. Thanks you very much!
+
 ## License
 
 This project is open-source and available under the [MIT License](https://opensource.org/licenses/MIT).
@@ -24,3 +35,4 @@ Happy Coding!
 [roadmap-frontend-projects]: https://roadmap.sh/frontend/projects
 [temperature-converter]: https://roadmap.sh/projects/temperature-converter
 [demo-link]: https://pine1611.github.io/frontend-projects/20-temperature-converter/public
+[be03]: https://github.com/Pine1611/backend-projects/blob/main/03-unit-converter/README.md
--- 20-temperature-converter/public/assets/js/scripts.js
+++ 20-temperature-converter/public/assets/js/scripts.js
@@ -10,12 +10,102 @@ class ConverterTemperature {
         selectList: ".select-js",
         selectListLabel: ".select__list-js",
         selectListInput: ".select__current-js",
+        valueConvert: "valueConvert",
+        btnExecute: "btnConvert",
     };
     static SIGN_DEGREE = "°";
+
+    static DATA_CONVERT_COLLECTED = { fromUnit: "K", toUnit: "K", valueConvert: 0, freezePoint: 0, absZero: 0 };
     static RESULT = document.getElementById("valueConverted");
 
     constructor() {
+        this.dataConvert = ConverterTemperature.DATA_CONVERT_COLLECTED;
+        this.controls = ConverterTemperature.CONTROL_CLASSES;
+        this.detailUnits = {
+            from: this.getDetailUnits(this.dataConvert.fromUnit),
+            to: this.getDetailUnits(this.dataConvert.toUnit),
+        };
+        this.resultEle = document.getElementById("result-js");
+
         this.initUnitSystem();
+        this.prepareData();
+        this.converterExecute();
+    }
+
+    prepareData() {
+        // add event for select units
+        const fromUnits = document.querySelectorAll("input[name=fromUnit]");
+        const toUnits = document.querySelectorAll("input[name=toUnit]");
+        fromUnits.forEach((item) => {
+            item.addEventListener("change", (event) => {
+                this.dataConvert.fromUnit = event.target.value; // set to collect data
+                this.detailUnits.from = this.getDetailUnits(event.target.value);
+            });
+        });
+        toUnits.forEach((item) => {
+            item.addEventListener("change", (event) => {
+                this.dataConvert.toUnit = event.target.value; // set to collect data
+                this.detailUnits.to = this.getDetailUnits(event.target.value);
+            });
+        });
+
+        const msgError = document.getElementById("invalidValueConvert");
+        const inputConvert = document.getElementById(this.controls.valueConvert);
+        inputConvert.addEventListener("change", (event) => {
+            if (isNaN(inputConvert.value)) {
+                msgError.classList.add("form__error-msg--show");
+                inputConvert.classList.add("form__error");
+                msgError.innerHTML = "Invalid value convert! Must be a number!";
+            } else {
+                msgError.classList.remove("form__error-msg--show");
+                inputConvert.classList.remove("form__error");
+                this.dataConvert.valueConvert = inputConvert.value;
+            }
+        });
+    }
+
+    converterExecute() {
+        const execute = document.getElementById(this.controls.btnExecute);
+        execute.addEventListener("click", (event) => {
+            let objConvertData = {
+                toUnit: this.dataConvert.toUnit,
+                valueConvert: Number(this.dataConvert.valueConvert),
+                fromFreezePoint: this.detailUnits.from.freezePoint,
+                toFreezePoint: this.detailUnits.to.freezePoint,
+            };
+
+            let result = 0;
+
+            if (this.dataConvert.fromUnit == "C") {
+                result = this.celsiusConverter(objConvertData);
+            } else if (this.dataConvert.fromUnit == "K") {
+                result = this.kelvinConverter(objConvertData);
+            } else {
+                result = this.fahrenheitConverter(objConvertData);
+            }
+
+            this.resultEle.innerHTML = "";
+            if (result === result && result % 1 !== 0) {
+                this.resultEle.innerHTML = Number(result.toFixed(6));
+            } else {
+                this.resultEle.innerHTML = result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            }
+        });
+    }
+
+    getDetailUnits(unit) {
+        let dataTemp = ConverterTemperature.TEMPERATURE_SYSTEM;
+
+        for (let i = 0; i < dataTemp.length; i++) {
+            if (dataTemp[i]["symbol"] === unit) {
+                let detailUnit = {
+                    absZero: dataTemp[i]["absZero"],
+                    freezePoint: dataTemp[i]["freezePoint"],
+                };
+                return detailUnit;
+            }
+        }
+        return -1;
     }
 
     // setup create list node - use for all dynamic load list select
@@ -27,6 +117,12 @@ class ConverterTemperature {
         let text = document.createTextNode(textLabel);
         label.setAttribute("for", `${target}-${symbol}`);
         label.classList.add(`${target}_select-js`);
+
+        label.addEventListener("click", (event) => {
+            this.resetListActive(`${target}_select-js`);
+            event.target.classList.add("active");
+        });
+
         li.appendChild(label);
         label.appendChild(text);
         return li;
@@ -47,9 +143,12 @@ class ConverterTemperature {
         input.type = "radio";
         input.id = `${target}-${symbol}`;
         input.value = symbol;
-        input.name = `unit-${target}`;
-        checkedDefault == "checked" ? (input.checked = "checked") : false;
+        input.name = `${target}Unit`;
+        checkedDefault == "checked" ? input.setAttribute("checked", "") : "";
+
         // setup label
+        label.setAttribute("for", `${target}-${symbol}`);
+
         label.appendChild(text);
         div.appendChild(input);
         div.appendChild(label);
@@ -58,7 +157,7 @@ class ConverterTemperature {
     }
 
     initUnitSystem() {
-        let classesListControl = ConverterTemperature.CONTROL_CLASSES;
+        const classesListControl = ConverterTemperature.CONTROL_CLASSES;
         const selectList = document.querySelectorAll(classesListControl.selectList);
         const selectInput = document.querySelectorAll(classesListControl.selectListInput);
         const selectLabel = document.querySelectorAll(classesListControl.selectListLabel);
@@ -69,16 +168,11 @@ class ConverterTemperature {
             selectLabel[indexItem].innerHTML = "";
 
             ConverterTemperature.TEMPERATURE_SYSTEM.forEach((unit, indexUnit) => {
-                let nodeLabel = this.createListNode(unit.name, unit.symbol, targetData);
+                let nodeList = this.createListNode(unit.name, unit.symbol, targetData);
                 let nodeInput = this.createInputListNode(unit.symbol, targetData, indexUnit == 0 ? "checked" : "");
 
-                nodeLabel.addEventListener("click", (event) => {
-                    this.resetListActive(`${targetData}_select-js`);
-                    event.target.classList.add("active");
-                });
-
+                selectLabel[indexItem].appendChild(nodeList);
                 selectInput[indexItem].appendChild(nodeInput);
-                selectLabel[indexItem].appendChild(nodeLabel);
             });
         });
     }
@@ -88,6 +182,38 @@ class ConverterTemperature {
         listSelectItems.forEach((item) => {
             item.classList.remove("active");
         });
+    }
+
+    // functions converter
+    celsiusConverter({ toUnit, valueConvert, fromFreezePoint, toFreezePoint }) {
+        switch (toUnit) {
+            case "K":
+                return valueConvert + toFreezePoint;
+            case "F":
+                return valueConvert * 1.8 + toFreezePoint;
+            default:
+                return valueConvert;
+        }
+    }
+    kelvinConverter({ toUnit, valueConvert, fromFreezePoint, toFreezePoint }) {
+        switch (toUnit) {
+            case "C":
+                return valueConvert - fromFreezePoint;
+            case "F":
+                return (valueConvert - fromFreezePoint) * 1.8 + toFreezePoint;
+            default:
+                return valueConvert;
+        }
+    }
+    fahrenheitConverter({ toUnit, valueConvert, fromFreezePoint, toFreezePoint }) {
+        switch (toUnit) {
+            case "C":
+                return (valueConvert - fromFreezePoint) / 1.8;
+            case "K":
+                return (valueConvert - fromFreezePoint) / 1.8 + toFreezePoint;
+            default:
+                return valueConvert;
+        }
     }
 }
 
